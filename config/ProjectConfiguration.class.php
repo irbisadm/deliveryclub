d8469e080dbc86c518a61c543feeb1b2f756858e
irbisadm <i@irbisadm.ru>
2013-07-13 11:36:24 +0000

    init

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
