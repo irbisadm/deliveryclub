d48753dd49f87bbc4303b6b8ef595a36ff988711
irbisadm <irbisadm@gmail.com>
2013-09-22 23:57:56 +0400

    templates

--- src/DC/AdminBundle/Resources/config/routing.php
+++ src/DC/AdminBundle/Resources/config/routing.php
@@ -8,6 +8,17 @@ $collection = new RouteCollection();
 $collection->add('dc_admin_homepage', new Route('/', array(
     '_controller' => 'DCAdminBundle:Default:index',
 )));
- 
+
+$collection->add('dc_admin_manageusers', new Route('/users', array(
+    '_controller' => 'DCAdminBundle:Users:list',
+)));
+
+$collection->add('dc_admin_addusers', new Route('/user', array(
+    '_controller' => 'DCAdminBundle:Users:create',
+)));
+
+$collection->add('dc_admin_editusers', new Route('/user/{id}', array(
+    '_controller' => 'DCAdminBundle:Users:edit',
+)));
 
 return $collection;
